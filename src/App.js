f1aea44993c3e4ccde2b6a8b1ea8086dfcdad322
rsmith1220 <rebecca.smith1220.rs@gmail.com>
2022-03-24 10:31:05 -0600

    Calendario solito
    A la pagina default de react se le agregó un llink que lleva a una página donde hay un calendario.

--- src/App.js
+++ src/App.js
@@ -24,13 +24,10 @@ function MyApp() {
 function App() {
   
   return (
-<<<<<<< Updated upstream
-    <div>App!</div>
-=======
+
 
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -48,7 +45,6 @@ function App() {
         
       </header>
     </div>
->>>>>>> Stashed changes
   );
 }
 
