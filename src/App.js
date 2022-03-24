06cb82b1cbdb0e73530e80ef9cf1f14df0c45a48
rsmith1220 <rebecca.smith1220.rs@gmail.com>
2022-03-24 10:24:54 -0600

    Calendario solito
    A la pagina default de react se le agregó un llink que lleva a una página donde hay un calendario.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
 import './App.css';
 
-function App() {
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import React, { useState } from "react";
+
+
+
+
+
+function MyApp() {
+  const [value, onChange] = useState(new Date());
+
   return (
+    <div>
+      <Calendar onChange={onChange} value={value} />
+    </div>
+  );
+  
+}
+
+
+
+function App() {
+  
+  return (
+<<<<<<< Updated upstream
     <div>App!</div>
+=======
+
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="calendario.js"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Calendario
+        </a>
+       
+        
+        
+        
+      </header>
+    </div>
+>>>>>>> Stashed changes
   );
 }
 
-export default App;
+//export default App;
+export default MyApp;
---
+++ src/calendar.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+
+function MyApp() {
+  const [value, onChange] = useState(new Date());
+
+  return (
+    <div>
+      <Calendar onChange={onChange} value={value} />
+    </div>
+  );
+}
+
+export default App;
