3189d9e5d93cb4d8d1b77b07a772c286d3725c14
Guillermo Santos <guillermo.santosba@gmail.com>
2022-03-25 15:40:57 -0600

    Finished login screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import './App.css';
 import Login from './pages/Login';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 function App() {
+  const theme = extendTheme({
+    colors: {
+      primary: "#6C8CBF",
+      secondary: "#FEF1B9",
+      background: "#F2F6FF",
+      textDark: "#1F1338",
+      textLight: "#fff"
+    },
+  });
   return (
-    <Login />
+    <ChakraProvider theme={theme}>
+      <Login />
+    </ChakraProvider>
   );
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
+import {
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Link,
+  VStack,
+} from "@chakra-ui/react";
+import { EmailIcon, LockIcon } from "@chakra-ui/icons";
+import blob from "../assets/blob01.png";
+
+// import colors from "../utils/colors";
 
 function Login() {
-    return (
-        <div>Login APP</div>
-    )
+  return (
+    <>
+      <header className="header">
+        <VStack spacing={"38px"} alignItems={"start"}>
+          <Heading as={"h1"} fontWeight={"light"}>
+            Que bueno verte de nuevo
+          </Heading>
+          <Heading as={"h2"} size={"m"}>
+            Ingresa tus credenciales
+          </Heading>
+        </VStack>
+        <Image
+          position={"absolute"}
+          right="0"
+          top="-10"
+          className="header__image"
+          src={blob}
+        />
+      </header>
+      <div className="form">
+        <VStack spacing={"24px"} width="30%" alignItems={"start"}>
+          <InputGroup>
+            <InputLeftElement pointerEvents={"none"} children={<EmailIcon />} />
+            <Input placeholder="Correo" />
+          </InputGroup>
+          <InputGroup>
+            <InputLeftElement pointerEvents={"none"} children={<LockIcon />} />
+            <Input placeholder="Contraseña" type={"password"} />
+          </InputGroup>
+          <Link color={"primary"}>¿Olvidaste tu contraseña?</Link>
+          <Button
+            display={"inline-block"}
+            backgroundColor="primary"
+            textColor="textLight"
+          >
+            Entrar
+          </Button>
+        </VStack>
+      </div>
+    </>
+  );
 }
 
-export default Login
+export default Login;
