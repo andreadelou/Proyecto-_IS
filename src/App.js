8530eeb05cab1207c82f8efc72f3356e7ab5bf5c
Jessica Ortiz <70552512+JessicaOI@users.noreply.github.com>
2022-03-24 23:03:57 -0600

    terminada plantilla pagina Health
    no es funcional aun porque debe de conectarse con las metas cumplidas por el usuario pero ya es la base

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './CSS/App.css';
+import Health from './pages/Health';
 
 
 function App() {
   return (
-    <div>App!</div>
+      <div>App!</div>
+      
   );
 }
 
--- src/pages/Health.js
+++ src/pages/Health.js
@@ -8,10 +8,43 @@ function Health() {
     return (
         <div className = "todo">
             <body>
+            
             <img className = "uno" src = {imgfondo} alt='uno' />
             <img className = "dos" src = {imgfondo} alt='dos' />
+            
             <h2>Salud</h2>
-            <p>Estadísticas de tus metas</p>
+            <p className = "desc">Estadísticas de tus metas</p>
+            <h3 className = "estadis1">Ejercicio</h3>
+            <h3 className = "estadis2">Leer</h3>
+            <h3 className = "estadis3">Tomar tus medicinas</h3>
+            <h3 className = "estadis4">Meditar</h3>
+
+            <p className = "cont1">25/30 días</p>
+            <p className = "cont2">29/30 días</p>
+            <p className = "cont3">29/30 días</p>
+            <p className = "cont4">15/30 días</p>
+
+            <div className = "amarillo1">
+
+                <div className = "azul1"></div>
+                
+            </div>
+            <div className = "amarillo2">
+
+                <div className = "azul2"></div>
+                
+            </div>
+            <div className = "amarillo3">
+
+                <div className = "azul3"></div>
+                
+            </div>
+            <div className = "amarillo4">
+
+                <div className = "azul4"></div>
+                
+            </div>
+            
             </body>
         </div>
     )
