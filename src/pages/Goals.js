2ccba00af07f97b829f55428b5c615b3a4daec39
Guillermo Santos <guillermo.santosba@gmail.com>
2022-04-13 20:33:56 -0600

    Fixed conflicts

--- src/pages/Goals.js
+++ src/pages/Goals.js
@@ -1,59 +1,60 @@
-import React, {useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TodoForm from "../components/TodoForm.js";
 import TodoList from "../components/TodoList.js";
 import "../Goals.css";
 
-const LOCAL_STORAGE_KEY ="react-todo-list-todos";
+const LOCAL_STORAGE_KEY = "react-todo-list-todos";
 
 function Goals() {
     const [todos, setTodos] = useState([]);
 
-    useEffect(()=> {
+    useEffect(() => {
         const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-        if(storageTodos){
-            setTodos(storageTodos);              
+        if (storageTodos) {
+            setTodos(storageTodos);
         }
-    },[todos]);
+    }, []);
 
     function addTodo(todo) {
         setTodos([todo, ...todos]);
     }
 
-    useEffect(()=> {
+    useEffect(() => {
         localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
-    },[todos]);
+    }, [todos]);
 
     function addTodo(todo) {
         setTodos([todo, ...todos]);
     }
 
-    function toggleComplete(id){
+    function toggleComplete(id) {
         setTodos(
-            todos.map(todo =>{
-                if(todo.id == id){
-                    return{
+            todos.map(todo => {
+                if (todo.id == id) {
+                    return {
                         ...todo,
                         completed: !todo.completed
                     };
                 }
                 return todo;
             })
-        ); 
+        );
     }
 
-    function removeTodo(id){
-        setTodos(todos.filter(todo => todo.id !==id));
+    function removeTodo(id) {
+        setTodos(todos.filter(todo => todo.id !== id));
     }
 
     return (
         <div className="Goals">
+
             <header className="Goals-header">
                 <p>Metas</p>
                 <TodoForm addTodo={addTodo} />
-                <TodoList 
-                todos={todos} 
-                toggleComplete={toggleComplete}
-                removeTodo={removeTodo} 
+                <TodoList
+                    todos={todos}
+                    toggleComplete={toggleComplete}
+                    removeTodo={removeTodo}
                 />
             </header>
         </div>
