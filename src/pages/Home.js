bd9388083d1b25d469f7d0506a69291157c57873
rebecca-s-m <rebecca.smith1220.rs@gmail.com>
2022-04-19 17:57:20 -0600

    Home page
    solo arregle el boton de log out y el titulo. agregue el fondo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,22 @@ import { auth, logout } from '../firebase.js';
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import { Button } from '@chakra-ui/react';
-
+import "../CSS/Hom.css";
+import blob from "../assets/blob01.png";
+import Header from "../components/Header";
+import {
+    Flex,
+    Heading,
+    Image,
+    Input,
+    InputGroup,
+    InputLeftElement,
+    Link,
+    VStack,
+    useToast,
+    HStack,
+    Spinner
+  } from "@chakra-ui/react";
 
 function Home() {
     const navigate = useNavigate(); // navigate
@@ -15,10 +30,18 @@ function Home() {
         if (!user) return navigate("/");
     }, [user, loading]);
     return (
-        <>
-            <div>Home</div>
-            <Button onClick={logout}>Logout</Button>
-        </>
+        <div className='pagina'>
+        <header><Button className='botoncitos' onClick={logout}>Logout</Button>
+        <Header title="Home" ></Header>
+        <Image
+          position={"absolute"}
+          right="0"
+          top="0"
+          className="header__image"
+          src={blob}
+        /></header>
+            
+        </div>
     )
 }
 
