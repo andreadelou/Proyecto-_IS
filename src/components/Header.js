919d7dc19f09db79276ba1344145c71a18929b18
Guillermo Santos <guillermo.santosba@gmail.com>
2022-04-19 16:02:53 -0600

    Implemented layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,21 @@ import React from 'react'
 
 function Header({ title, subtitle }) {
     return (
-        <VStack spacing={"38px"} alignItems={"start"}>
-            <Heading as={"h1"} fontSize={"70px"} fontWeight={"300"}>
-                {title}
-            </Heading>
-            <Heading as={"h2"} fontSize={'25px'} >
-                {subtitle}
-            </Heading>
-        </VStack>
+        <div style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "flex-start"
+        }}>
+            <VStack spacing={"38px"} alignItems={"start"}>
+                <Heading as={"h1"} fontSize={"70px"} fontWeight={"300"}>
+                    {title}
+                </Heading>
+                <Heading as={"h2"} fontSize={'25px'} >
+                    {subtitle}
+                </Heading>
+            </VStack>
+            <nav>Hola</nav>
+        </div>
     )
 }
 
