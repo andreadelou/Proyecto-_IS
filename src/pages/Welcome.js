6baed5e7ef3c7ab92bf63fbcbdf97be89724c89b
rebecca-s-m <rebecca.smith1220.rs@gmail.com>
2022-04-20 18:52:07 -0600

    Agregaron botones e imagenes

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -3,9 +3,13 @@ import { auth, logout } from '../firebase.js';
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import { Button } from '@chakra-ui/react';
+import medicina from "../assets/droga-de-la-medicina.png";
+import leer from "../assets/leer.png";
+import meditar from "../assets/meditacion.png";
+import trotar from "../assets/trotar.png";
 import blob from "../assets/blob01.png";
 import Header from "../components/Header";
-import "../CSS/Hom.css"
+import "../CSS/welcome.css"
 import {
     Flex,
     Heading,
@@ -31,8 +35,11 @@ function Welcome() {
     }, [user, loading]);
     return (
         <div className='pagina'>
-        <header><Button className='botones' onClick={logout}>Logout</Button>
+        <header>
         <Header title="Bienvenidx" ></Header>
+        <span className='subtitulos'>Escoge tus metas para comenzar</span>
+        <p></p>
+        <span className='subtitulos'>Elige como minimo dos</span>
         <Image
           position={"absolute"}
           right="0"
@@ -40,6 +47,11 @@ function Welcome() {
           className="header__image"
           src={blob}
         /></header>
+
+        <button className='botonesnow' ><img src={trotar} height ="40" width="60" />Ejercicio </button>
+        <button className='botonesnow' ><img src={meditar} height ="40" width="60" />Meditar </button>
+        <button className='botonesnow' ><img src={leer} height ="40" width="60" />Leer </button>
+        <button className='botonesnow' ><img src={medicina} height ="40" width="60" />Medicina </button>
             
         </div>
     )
