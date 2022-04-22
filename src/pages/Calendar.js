024e060bb579663aac31085eadd439933782b9bb
Guillermo Santos <guillermo.santosba@gmail.com>
2022-04-22 09:44:27 -0600

    Implemented calendar

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,14 +1,27 @@
 import { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import Header from '../components/Header';
 import "../CSS/Calendario.css";
+import blob from "../assets/blob01.png";
+import { Image } from '@chakra-ui/react';
 
 function Calendario() {
   const [date, setDate] = useState(new Date());
 
   return (
-    <div className='app'>
-      <h1 className='text-center'>Calendario de este mes</h1>
+    <div className='calendar'>
+      <header className="header">
+        <Header title="Calendario" subtitle="Organiza tu tiempo"></Header>
+        <Image
+          position={"absolute"}
+          right="0"
+          top="0"
+          className="header__image"
+          src={blob}
+        />
+      </header>
+
       <div className='calendar-container'>
         <Calendar onChange={setDate} value={date} />
       </div>
