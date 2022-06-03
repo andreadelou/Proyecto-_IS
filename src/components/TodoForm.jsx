dac8c500c60ad050d6dc269a65fd40f68e66005d
andreadelou <70552370+andreadelou@users.noreply.github.com>
2022-06-03 00:35:27 -0600

    Revert "Merge branch 'prueba_funcionalidad' into feature/proxima-meta"
    This reverts commit 0602819a4940e73c681a30e76612d8b4ffc5fb3a, reversing
    changes made to 48f651622893c6b3b62aa2050a1d436a45985cfe.

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,19 +1,11 @@
 //import { usePinInputDescendant } from "@chakra-ui/react";
-import { HStack } from "@chakra-ui/react";
 import React, { useState } from "react";
 import "../CSS/TodoForm.css";
 
 import AddButton from "./AddButton";
 import EditButton from "./EditButton";
 
-function TodoForm({
-  title,
-  onAdd,
-  completed,
-  onToggleCompleted,
-  onEdit,
-  percentage = 0,
-}) {
+function TodoForm({ title, onAdd, completed, onToggleCompleted, onEdit }) {
   const [todo, setTodo] = useState({
     id: "",
     task: "",
@@ -21,41 +13,25 @@ function TodoForm({
   });
 
   return (
-    <>
-      <div className="todo-form">
-        <div className="todo-form__goal">
-          <div className="todo-form__title">
-            <input
-              type="checkbox"
-              defaultChecked={completed}
-              onChange={onToggleCompleted}
-              name="goal-checkbox"
-              className="todo-form__check"
-            />
-            <h1
-              type="text"
-              name="goal-title"
-              placeholder="Nombre de tu meta"
-              className="todo-form__input"
-            >
-              {title}
-            </h1>
-          </div>
-          <div className="todo-form__progress-bar">
-            <div
-              className="todo-form__progress-bar--progress"
-              style={{
-                width: `${percentage}%`,
-              }}
-            ></div>
-          </div>
-        </div>
-        <HStack marginLeft={10}>
-          <AddButton onAdd={onAdd} />
-          <EditButton onEdit={onEdit} />
-        </HStack>
-      </div>
-    </>
+    <div className="todo-form">
+      <input
+        type="checkbox"
+        defaultChecked={completed}
+        onChange={onToggleCompleted}
+        name="goal-checkbox"
+        className="todo-form__check"
+      />
+      <h1
+        type="text"
+        name="goal-title"
+        placeholder="Nombre de tu meta"
+        className="todo-form__input"
+      >
+        {title}
+      </h1>
+      <AddButton onAdd={onAdd} />
+      <EditButton onEdit={onEdit} />
+    </div>
   );
 }
 
--- src/services/goals.service.js
+++ src/services/goals.service.js
@@ -66,6 +66,13 @@ export const updateGoalTodo = async (goal, todoIndex, updatedTodo) => {
  * @param {*} goal 
  */
 export const updateGoal = async (goalId, updatedGoal) => {
+    // goals = goals.map((goal) => {
+    //     if (goal.id === goalId) {
+    //         goal = updatedGoal;
+    //     }
+    //     return goal;
+    // });
+    // saveGoalsInLocal();
     const docRef = doc(db, 'goals', goalId);    // Get the document reference from firebase
     await updateDoc(docRef, updatedGoal);   // Update the document
 }
@@ -102,8 +109,7 @@ export const insertGoal = async (title, category, reminder = '') => {
         createdAt: (new Date()).toISOString(),
         todos: [
 
-        ],
-        progress: 0
+        ]
     });
 }
 
@@ -129,15 +135,7 @@ export const fetchAllGoals = async () => {
 export const proximatarea = async () => {
     const uid = auth.currentUser.uid;
     const goalsCol = collection(db, 'goals');
-    const q = query(goalsCol, where("uid", "==", uid),where("completed","==",false),orderBy("reminder","desc"),limit(1))}
-/**
- * Fetch most recent goals
- */
-export const fetchExpiredTasks = async () => {
-    const uid = auth.currentUser.uid;
-
-    const goalsCol = collection(db, 'goals');
-    const q = query(goalsCol, where("uid", "==", uid), where('completed', "==", false), orderBy('reminder', 'asc'), limit(1))
+    const q = query(goalsCol, where("uid", "==", uid),where("completed","==",false),orderBy("reminder","desc"),limit(1))
     const goalsSnapshot = await getDocs(q);
     return goalsSnapshot.docs.map(d => {
         return {
@@ -145,7 +143,6 @@ export const fetchExpiredTasks = async () => {
             ...d.data(),
         }
     }) ?? [];
-
 }
 
 export const fetchAllGoalsAndGroupByCategory = async () => {
