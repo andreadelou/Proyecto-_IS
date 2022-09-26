745bd221e9f5f2d266b13087bf790497ae89bdfc
Guillermo Santos <guillermo.santosba@gmail.com>
2022-09-25 20:57:38 -0600

    ✅ Implemented test that validates if the save method is called.

--- frontend/src/__tests__/goals.test.jsx
+++ frontend/src/__tests__/goals.test.jsx
@@ -37,4 +37,39 @@ describe("Tests for the goals logic", () => {
 		expect(saveButton.closest('button').disabled).toBeFalsy()
 	})
 
+		
+	test('Saves the goal when the user clicks the button.', async () => {
+		// Arrange
+		const onOpenMock = jest
+			.fn()
+			.mockName('onOpen')
+		const onCloseMock = jest
+			.fn()
+			.mockName('onClose')
+		const onSaveMock = jest
+			.fn()
+			.mockName('onSave')
+		
+		await act(async() => {
+			render(
+				<GoalModal
+					isOpen={true} onOpen={onOpenMock}
+					onSave ={onSaveMock}
+					onClose={onCloseMock} />,
+				{ wrapper: BrowserRouter }
+			)  
+		})
+		const titleInput = screen.getByTestId('title')
+		const categoryInput = screen.getByTestId('category')
+		const reminderInput = screen.getByTestId('reminder')
+		const saveButton = screen.getByTestId('save')
+		// Act
+		fireEvent.change(titleInput, { target: { value: 'Goal Title' } })
+		fireEvent.change(categoryInput, { target: { value: 'health' } })
+		fireEvent.change(reminderInput, { target: { value: '2022/09/27' } })
+		fireEvent.click(saveButton)
+		// Assert
+		expect(onSaveMock).toHaveBeenCalled()
+	})
+
 })
