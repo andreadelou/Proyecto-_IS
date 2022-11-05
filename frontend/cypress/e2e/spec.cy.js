4f78427a44723bc1baf4a3d2c17788f99922b788
Andrea Lam <70552370+andreadelou@users.noreply.github.com>
2022-11-05 12:03:11 -0600

    registrar correo
    Este aun no funciona

--- frontend/cypress/e2e/spec.cy.js
+++ frontend/cypress/e2e/spec.cy.js
@@ -1,7 +1,17 @@
 describe('My First Test', () => {
-  it('clicks the link "type"', () => {
-    cy.visit('https://example.cypress.io')
+  it('Rueba crear meta', () => {
+    cy.visit('https://mind-app-b0b0f.web.app/#/')
 
-    cy.contains('type').click()
+    // cy.contains('type').click()
+
+    // Should be on a new URL which
+    // includes '/commands/actions'
+    cy.url().should('include', '/commands/actions')
+
+    // Get an input, type into it and verify
+    // that the value has been updated
+    cy.get('.action-email')
+      .type('lam20102@uvg.edu.gt')
+      .should('have.value', 'lam20102@uvg.edu.gt')
   })
 })
