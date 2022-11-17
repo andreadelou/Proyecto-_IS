5cb809f40b2c140a0e22539e572eba6e6c203ef1
Andrea Lam <70552370+andreadelou@users.noreply.github.com>
2022-11-16 18:11:58 -0600

    Update tienda.cy.js

--- frontend/cypress/e2e/tienda.cy.js
+++ frontend/cypress/e2e/tienda.cy.js
@@ -1,5 +1,86 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Logging In - Basic Auth', function () {
+  // we can use these values to log in
+  const username = 'lam20102@uvg.edu.gt'
+  const password = 'princesa71'
+
+  
+
+  context('cy.request', () => {
+    // https://on.cypress.io/request
+
+    it('without authorization gets 401', () => {
+      cy.request({
+        url: 'https://mind-app-b0b0f.web.app/#/',
+        failOnStatusCode: false,
+      }).its('status').should('equal', 200)
+    })
+
+    it('with authorization', () => {
+      cy.request({
+        url: 'https://mind-app-b0b0f.web.app/#/',
+        auth: {
+          username, password,
+        },
+      }).its('status').should('equal', 200)
+    })
+
   })
-})
+
+  context('cy.visit', () => {
+    // https://on.cypress.io/visit
+
+    it('loads the page using basic auth', () => {
+      cy.visit('https://mind-app-b0b0f.web.app/#/', {
+        auth: {
+          username,
+          password,
+        },
+      })
+
+      // confirm that all static resources have loaded
+      cy.get('#app-message').should('not.be.empty')
+      cy.log('app.js loaded')
+
+      // cy.contains('h1', 'Red').should('have.css', 'color', 'rgb(255, 0, 0)')
+      cy.log('app.css loaded')
+    })
+  })
+
+  context('logins', () => {
+    // https://on.cypress.io/visit
+
+    it('successfully logs in ', () => {
+      cy.intercept('GET', `${Cypress.env('apiUrl')}https://mind-app-b0b0f.web.app/#/`)
+        .as('getUserModels')
+    
+      cy.request('POST', `https://mind-app-b0b0f.web.app/#/`, {
+        username: Cypress.env(username),
+        password: Cypress.env(password),
+      }).then((response) => {
+        cy.setCookie('auth_key', '123key')
+      })
+    
+      cy.visit('https://mind-app-b0b0f.web.app/#/home')
+      // cy.wait('@getUserModels')
+      cy.contains('h1', 'Home').should('be.visible')
+    })
+
+    
+    })
+
+    context('Comprar', () => {
+      // https://on.cypress.io/visit
+      
+      it('successfully logs in ', () => {
+
+        cy.contains('Personalizar').click()
+        cy.contains('Tienda').click()
+        cy.get('[alt="cuadro1"]').click()
+        
+      })
+
+      // cy.contains('Welcome').click()
+  
+      
+      })
+  })
