5a4185e00ea81f2113fe32117894f07e9fed9038
Guillermo Santos <guillermo.santosba@gmail.com>
2022-11-21 15:23:45 -0600

    Added coverage ignore

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './CSS/index.css';
-import App from './App';
+import App from './App.js';
 import { HashRouter as Router, } from 'react-router-dom';
 
 ReactDOM.render(
